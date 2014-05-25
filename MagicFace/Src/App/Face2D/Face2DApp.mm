f5860adfdb2fb5f08872ac7bf605b22a2c9774f3
liguo <liguo.magic3d@gmail.com>
2014-05-25 08:58:02 +0800

    Update MagicLib.

--- MagicFace/Src/App/Face2D/Face2DApp.mm
+++ MagicFace/Src/App/Face2D/Face2DApp.mm
@@ -8,8 +8,31 @@
 
 #import "Face2DApp.h"
 #import "Face2D.h"
+#import "Face2DObj.h"
+#import "MagicObjectManager.h"
+
+@interface Face2DApp()
+{
+    MagicApp::Face2DObj* mF2DObj;
+}
+@end
 
 @implementation Face2DApp 
+
+- (BOOL)Enter
+{
+    if (!MOMGR->IsObjExist("Face2DObj"))
+    {
+        MOMGR->InsertObj("Face2DObj", new MagicApp::Face2DObj);
+    }
+    mF2DObj = dynamic_cast<MagicApp::Face2DObj*>(MOMGR->GetObj("Face2DObj"));
+    return YES;
+}
+
+- (BOOL)Exit
+{
+    return YES;
+}
 
 - (NSImage*)LoadImage:(NSString*)filePath
 {
